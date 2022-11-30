45c2cf64218933229e450a24066fd60a876f327b
Cristhian Cantero <cantero.cristhian@gmail.com>
2022-11-30 00:02:10 -0300

    ExchangeRate Apps ListView

--- currencyconverter/views.py
+++ currencyconverter/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+
+from currencyconverter.models import ExchangeRate
 
 # Create your views here.
+class ListExchangeRates(generic.ListView):
+    model = ExchangeRate
